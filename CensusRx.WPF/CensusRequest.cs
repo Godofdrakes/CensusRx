db5a38f3485a47ce9656c3d0b9f7d0c791bcc8ea
Godofdrakes <godofdrakes@gmail.com>
2023-03-15 17:02:57 -0700

    Refactor

--- CensusRx.WPF/CensusRequest.cs
+++ CensusRx.WPF/CensusRequest.cs
@@ -1,46 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using CensusRx.Interfaces;
 using CensusRx.WPF.Interfaces;
 
 namespace CensusRx.WPF;
 
-public sealed class CensusRequest<T> : ICensusRequest<T>
+public sealed class CensusRequest<T> : ICensusRequestBuilder<T>
 	where T : ICensusObject
 {
-	private string? TempString { get; set; }
-
-	private readonly List<(string key, CensusMatch value)> _queryParams = new();
-
-	public IReadOnlyList<(string key, CensusMatch value)> QueryParams => _queryParams;
-
-	public ICensusRequest<T> Where(string query)
+	private class MatchBuilder : ICensusMatchBuilder<T>
 	{
-		if (TempString is not null)
-			throw new InvalidOperationException("TempString is not null");
+		public CensusRequest<T> CensusRequest { get; }
 
-		TempString = query;
-		return this;
+		public string Query { get; }
+
+		public MatchBuilder(CensusRequest<T> censusRequest, string query)
+		{
+			CensusRequest = censusRequest;
+			Query = query;
+		}
+
+		public ICensusRequestBuilder<T> Matches(CensusMatch censusMatch)
+		{
+			CensusRequest.QueryParams.Add(Query, censusMatch);
+			return CensusRequest;
+		}
 	}
 
-	public ICensusRequest<T> Matches(CensusMatch censusMatch)
-	{
-		if (TempString is null)
-			throw new InvalidOperationException("TempString is null");
+	public Dictionary<string, CensusMatch> QueryParams { get; } = new();
 
-		_queryParams.Add((TempString, censusMatch));
-		TempString = null;
-		return this;
-	}
-
-	public ICensusRequest<T> Bind(Action<(string key, CensusMatch value)> bindAction)
-	{
-		if (TempString is not null)
-			throw new InvalidOperationException("TempString is not null");
-
-		foreach (var pair in this._queryParams)
-			bindAction.Invoke(pair);
-
-		return this;
-	}
+	public ICensusMatchBuilder<T> Where(string query) => new MatchBuilder(this, query);
 }
---
+++ CensusRx.WPF/Interfaces/ICensusRequestBuilder.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text.Json;
+using CensusRx.Interfaces;
+
+namespace CensusRx.WPF.Interfaces;
+
+public delegate void RequestBuilder<T>(ICensusRequestBuilder<T> request) where T : ICensusObject;
+
+internal sealed class PropertyVisitor : ExpressionVisitor
+{
+	public Stack<MemberInfo> PathStack { get; } = new();
+
+	public JsonNamingPolicy? NamingPolicy { get; set; }
+
+	protected override Expression VisitMember(MemberExpression node)
+	{
+		if (node.Member is not PropertyInfo propertyInfo)
+		{
+			throw new InvalidOperationException("Expression must be property chain");
+		}
+
+		PathStack.Push(propertyInfo);
+
+		return base.VisitMember(node);
+	}
+
+	public override string ToString()
+	{
+		string ConvertName(MemberInfo info) => NamingPolicy?.ConvertName(info.Name) ?? info.Name;
+		return string.Join(".", PathStack.Select(ConvertName));
+	}
+}
+
+public interface ICensusMatchBuilder<T> where T : ICensusObject
+{
+	ICensusRequestBuilder<T> Matches(CensusMatch censusMatch);
+
+	ICensusRequestBuilder<T> IsEqualTo(string value) => Matches(CensusMatch.IsEqualTo(value));
+	ICensusRequestBuilder<T> StartsWith(string value) => Matches(CensusMatch.StartsWith(value));
+}
+
+public interface ICensusRequestBuilder<T> where T : ICensusObject
+{
+	ICensusMatchBuilder<T> Where(string query);
+
+	ICensusMatchBuilder<T> Where<TProp>(Expression<Func<T, TProp>> expression)
+	{
+		var visitor = new PropertyVisitor
+		{
+			NamingPolicy = CensusJson.SerializerOptions.PropertyNamingPolicy
+		};
+
+		visitor.Visit(expression.Body);
+
+		return Where(visitor.ToString());
+	}
+
+	ICensusRequestBuilder<T> LimitTo(int count) => Where("c:limit").IsEqualTo(count.ToString());
+}
